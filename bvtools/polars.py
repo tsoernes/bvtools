b246fa365b0b8b5fb4383597b57d953629a8fcae
Torstein Sørnes <t.soernes@gmail.com>
2024-10-13 14:30:26 +0200

    Allow series; parallelize some code

--- bvtools/polars.py
+++ bvtools/polars.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env ipython
 """A collection of tools and utilities for Polars"""
 
 import polars as pl
@@ -7,13 +6,13 @@ import polars.selectors as cs
 
 def drop_empty_list_columns(df: pl.DataFrame, verbose: bool = True) -> pl.DataFrame:
     """Drop columns with just empty lists"""
-    for col in df.columns:
-        if isinstance(df[col].dtype, pl.List):
-            if df[col].list.len().sum() == 0:
-                df = df.drop(col)
-                if verbose:
-                    print(f"Dropped empty list column {col}")
-    return df
+    empty_cols = [col for col in df.columns if isinstance(df[col].dtype, pl.List) and df[col].list.len().sum() == 0]
+
+    if empty_cols and verbose:
+        for col in empty_cols:
+            print(f"Dropped empty list column {col}")
+
+    return df.drop(empty_cols) if empty_cols else df
 
 
 def drop_empty_list_columns_in_place(df: pl.DataFrame, verbose: bool = True) -> None:
@@ -27,11 +26,17 @@ def drop_empty_list_columns_in_place(df: pl.DataFrame, verbose: bool = True) -> 
 
 
 def empty_string_to_null(
-    df: pl.DataFrame, columns: str | list[str] | None = None, verbose: bool = True
-) -> pl.DataFrame:
+    df: pl.Series | pl.DataFrame, columns: str | list[str] | None = None, verbose: bool = True
+) -> pl.Series | pl.DataFrame:
     """
     Replace empty strings with nulls. If no column(s) are given then all suitable columns are processed.
     """
+    if isinstance(df, pl.Series):
+        is_series = True
+        df = df.to_frame()
+    else:
+        is_series = False
+
     if isinstance(columns, str):
         columns = [columns]
     # Select string columns
@@ -42,52 +47,54 @@ def empty_string_to_null(
 
     # Apply the mask to set empty strings to null
     df = df.with_columns(
-        [
             pl.when(masks[col]).then(None).otherwise(pl.col(col)).alias(col)
             for col in string_cols
-        ]
     )
 
     if verbose:
         for col in string_cols:
-            if verbose:
-                if n_empty := masks[col].sum():
-                    print(f"Replaced {n_empty} empty strings with null in column {col}")
+            if n_empty := masks[col].sum():
+                print(f"Replaced {n_empty} empty strings with null in column {col}")
+
+    if is_series:
+        df = df.to_series(0)
 
     return df
 
 
-def empty_string_to_null_in_place(
-    df: pl.DataFrame, columns: str | list[str] | None = None, verbose: bool = True
-):
-    """
-    Replace empty strings with nulls, in_place.
-    If no column(s) are given then all suitable columns are processed.
-    """
-    if isinstance(columns, str):
+def empty_list_to_null(
+    df: pl.DataFrame | pl.Series,
+    columns: str | list[str] | None = None,
+    verbose: bool = True
+) -> pl.DataFrame | pl.Series:
+    """Given a polars DataFrame, convert all empty lists in the given column(s) to nulls.
+    If no columns are specified, the all list columns will be processed."""
+    if isinstance(df, pl.Series):
+        is_series = True
+        df = df.to_frame()
+    else:
+        is_series = False
+
+    if columns is None:
+        columns = [col for col in df.columns if isinstance(df[col].dtype, pl.List)]
+    elif isinstance(columns, str):
         columns = [columns]
-    # Select string columns
-    string_cols = columns if columns else df.select(cs.by_dtype(pl.String)).columns
 
-    # Create a mask for empty strings
-    masks = {col: df[col].str.len_bytes().eq(0) for col in string_cols}
-
-    # Apply the mask to set empty strings to null
-    for col in string_cols:
-        df[col] = pl.when(masks[col]).then(None).otherwise(pl.col(col))
-        if verbose:
-            if n_empty := masks[col].sum():
-                print(f"Replaced {n_empty} empty strings with null in column {col}")
-
-
-def empty_list_to_null(df: pl.DataFrame) -> pl.DataFrame:
-    raise NotImplementedError("untested")
-    # Convert empty lists to None
     df = df.with_columns(
-        pl.when(cs.by_dtype(pl.List(pl.String)).list.len() > 0).then(
-            cs.by_dtype(pl.List(pl.String))
-        )
+        pl.when(pl.col(column).list.len().eq(0))
+        .then(None)
+        .otherwise(pl.col(column))
+        .alias(column)
+        for column in columns
     )
+
+    if verbose:
+        for column in columns:
+            if column not in df.columns:
+                print(f"{column=} not in df")
+    if is_series:
+        df = df.to_series(0)
+    return df
 
 
 def unnest_struct_columns(
@@ -102,19 +109,12 @@ def unnest_struct_columns(
     If no column(s) are given then all suitable columns are processed.
     """
     # Identify JSON columns by checking for struct type
-    if columns:
-        if isinstance(columns, str):
-            struct_columns = [columns]
-        else:
-            struct_columns = columns
+    if columns is None:
+        struct_columns = [col for col in df.columns if isinstance(df[col].dtype, pl.Struct)]
+    elif isinstance(columns, str):
+        struct_columns = [columns]
     else:
-        struct_columns = [
-            col for col in df.columns if isinstance(df[col].dtype, pl.Struct)
-        ]
-
-    # No JSON columns found
-    if not struct_columns:
-        return df
+        struct_columns = columns
 
     # Process each JSON column
     flattened_dfs = []
@@ -134,7 +134,7 @@ def unnest_struct_columns(
             flattened = unnest_struct_columns(flattened, recurse=True)
         if verbose:
             new_cols = ", ".join([f"'{c}'" for c in flattened.columns])
-            print(f"Flattened '{col}' to {new_cols}")
+            print(f"Unnested '{col}' to {new_cols}")
         flattened_dfs.append(flattened)
 
     # Combine all DataFrames horizontally
@@ -142,26 +142,28 @@ def unnest_struct_columns(
 
 
 def convert_date_columns(
-    df: pl.DataFrame, date_columns: list[str], format: str = "%Y-%m-%d"
+    df: pl.DataFrame, columns: str | list[str], format: str = "%Y-%m-%d"
 ) -> pl.DataFrame:
-    """Convert specified columns to date type."""
-    for col in date_columns:
-        if col in df.columns:
-            df = df.with_columns(
-                [pl.col(col).str.strptime(pl.Date, format=format).alias(col)]
-            )
-        else:
-            print(f"{col=} not in df")
+    """Convert (parse) specified string column(s) to date type."""
+    if isinstance(columns, str):
+        columns = [columns]
+
+    df = df.with_columns(
+        [pl.col(col).str.strptime(pl.Date, format=format).alias(col)]
+        for col in columns
+    )
     return df
 
 
 def cast_columns(
-    df: pl.DataFrame, int_columns: list[str], dtype=pl.Int32
+        df: pl.DataFrame, columns: list[str], dtype, verbose: bool = True,
 ) -> pl.DataFrame:
     """Convert specified columns to given type."""
-    for col in int_columns:
+    columns_ = []
+    for col in columns:
         if col in df.columns:
-            df = df.with_columns([pl.col(col).cast(dtype).alias(col)])
-        else:
+            columns_.append(col)
+        elif verbose:
             print(f"{col=} not in df")
+    df = df.with_columns([pl.col(col).cast(dtype).alias(col) for col in columns_])
     return df
